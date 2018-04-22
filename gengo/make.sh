72e18ebfddf6446b839eb8afb2ec2430d749d33b
tamal <tamal@appscode.com>
2018-04-22 07:17:43 -0700

    Create generator for kube 1.10

--- gengo/make.sh
+++ gengo/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build -t appscode/gengo:release-1.9 .
-docker push appscode/gengo:release-1.9
+docker build -t appscode/gengo:release-1.10 .
+docker push appscode/gengo:release-1.10
--- protoc/make.sh
+++ protoc/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build -t appscode/protoc:release-1.9 .
-docker push appscode/protoc:release-1.9
+docker build -t appscode/protoc:release-1.10 .
+docker push appscode/protoc:release-1.10
