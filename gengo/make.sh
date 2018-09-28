e5224d70a542d96c8250e02d59efee44adfb8d4c
Tamal Saha <tamal@appscode.com>
2018-09-28 00:59:31 -0700

    Support Kubernetes 1.12

--- gengo/make.sh
+++ gengo/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build -t appscode/gengo:release-1.11 .
-docker push appscode/gengo:release-1.11
+docker build --pull -t appscode/gengo:release-1.12 .
+docker push appscode/gengo:release-1.12
--- protoc/make.sh
+++ protoc/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build -t appscode/protoc:release-1.11 .
-docker push appscode/protoc:release-1.11
+docker build --pull -t appscode/protoc:release-1.12 .
+docker push appscode/protoc:release-1.12
