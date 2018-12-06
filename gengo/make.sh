49dc1e246797d40e52c07ac4f0dc0ecf8cc4108c
Tamal Saha <tamal@appscode.com>
2018-12-05 22:11:05 -0800

    Support Kubernetes 1.13

--- gengo/make.sh
+++ gengo/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build --pull -t appscode/gengo:release-1.12 .
-docker push appscode/gengo:release-1.12
+docker build --pull -t appscode/gengo:release-1.13 .
+docker push appscode/gengo:release-1.13
