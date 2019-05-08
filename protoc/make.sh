d8eab29f8a011f843abc7e929bfd4f1543da1a3b
Tamal Saha <tamal@appscode.com>
2019-05-07 21:58:00 -0700

    K8s 1.14.0

--- protoc/make.sh
+++ protoc/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build --pull -t appscode/protoc:release-1.13 .
-docker push appscode/protoc:release-1.13
+docker build --pull -t appscode/protoc:release-1.14 .
+docker push appscode/protoc:release-1.14
